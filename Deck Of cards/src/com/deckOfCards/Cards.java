b8406ff6935879417c4def05ab51cdf43ae3972d
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-26 09:58:45 +0530

    Added : Method to setup the card

---
+++ Deck Of cards/src/com/deckOfCards/Cards.java
@@ -0,0 +1,24 @@
+package com.deckOfCards;
+
+public class Cards {
+private String suit;
+private String rank;
+public Cards(String suit, String rank) {
+	this.suit = suit;
+	this.rank = rank;
+}
+public String getSuit() {
+	return suit;
+}
+public void setsuit() {
+	this.setsuit();
+}
+public String getrank() {
+	return rank;
+}
+public void setrank() {
+	this.setrank();
+}
+
+
+}
--- Deck Of cards/src/com/deckOfCards/deckOfCards.java
+++ Deck Of cards/src/com/deckOfCards/deckOfCards.java
@@ -1,5 +1,52 @@
 package com.deckOfCards;
 
-public class deckOfCards {
+import java.util.ArrayList;
 
+public class deckOfCards {
+	String suit[] = { "clubs", "diamonds", "hearts", "spades" };
+	String rank[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace" };
+	ArrayList<Cards> cards = new ArrayList<Cards>(52);
+	public static void main(String[] args) {
+	deckOfCards cards = new deckOfCards();
+	cards.setCard();
+	}
+
+	public void setCard() {
+		for(int i=0;i<=3;i++) {
+			for(int j=0;j<=12;j++) {
+				this.cards.add(new Cards (suit[i], rank[j]));
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+	}
 }
