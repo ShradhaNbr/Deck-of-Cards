f8eff8bb2d741dc6fc2efea36513df51b73f8e69
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-26 10:10:32 +0530

    Added : Method to setup the card

--- Deck Of cards/src/com/deckOfCards/Cards.java
+++ Deck Of cards/src/com/deckOfCards/Cards.java
@@ -1,24 +1,33 @@
 package com.deckOfCards;
 
 public class Cards {
-private String suit;
-private String rank;
-public Cards(String suit, String rank) {
-	this.suit = suit;
-	this.rank = rank;
-}
-public String getSuit() {
-	return suit;
-}
-public void setsuit() {
-	this.setsuit();
-}
-public String getrank() {
-	return rank;
-}
-public void setrank() {
-	this.setrank();
-}
+	private String suit;
+	private String rank;
 
+	public Cards(String suit, String rank) {
+		this.suit = suit;
+		this.rank = rank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setsuit(String suit) {
+		this.suit = suit;
+	}
+
+	public String getrank() {
+		return rank;
+	}
+
+	public void setrank(String rank) {
+		this.rank = rank;
+	}
+
+	@Override
+	public String toString() {
+		return "Card [suit=" + suit + ", rank=" + rank + "]";
+	}
 
 }
--- Deck Of cards/src/com/deckOfCards/deckOfCards.java
+++ Deck Of cards/src/com/deckOfCards/deckOfCards.java
@@ -3,50 +3,32 @@ package com.deckOfCards;
 import java.util.ArrayList;
 
 public class deckOfCards {
-	String suit[] = { "clubs", "diamonds", "hearts", "spades" };
-	String rank[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace" };
+	// ArrayList to store the values.
 	ArrayList<Cards> cards = new ArrayList<Cards>(52);
-	public static void main(String[] args) {
-	deckOfCards cards = new deckOfCards();
-	cards.setCard();
+
+	// Store the suits.
+	private static String[] suit = { "Clubs", "Diamonds", "Hearts", "Spades" };
+
+	// store the rank of card.
+	private static String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+	/*
+	 * Purpose: Method to setup the deck with unique cards.
+	 */
+	private void setupDeck() {
+		for (int i = 0; i < suit.length; i++) {
+			for (int j = 0; j < rank.length; j++) {
+				this.cards.add(new Cards(suit[i], rank[j]));
+			}
+		}
 	}
 
-	public void setCard() {
-		for(int i=0;i<=3;i++) {
-			for(int j=0;j<=12;j++) {
-				this.cards.add(new Cards (suit[i], rank[j]));
-			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		}
-		
+	public static void main(String[] args) {
+
+		deckOfCards deckOfCards = new deckOfCards();
+
+		deckOfCards.setupDeck();
+
 	}
+
 }
